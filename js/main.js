7489cf88fed19c5a5149ce553525fe5ce05ac3cf
tommy-yoon <taeil.yoon@gmail.com>
2021-10-04 22:30:33 +1300

    make all blog pages responsive

--- js/main.js
+++ js/main.js
@@ -4,15 +4,17 @@ $(function () {
     console.log("loaded")
     loadUpdates();
     window.addEventListener('resize', loadUpdates);
+    console.log($("#updates-mobile > ul"));
+    $("#updates-mobile").click(() => { $("#updates-mobile > ul").toggle(); })
 });
 
 function loadUpdates() {
     var x = window.matchMedia("(min-width: 700px)");
     console.log("(max-width: 350px)", x)
-    if (x.matches) { // If media query matches
+    if (x.matches) { // If media query matches ==> big screen
         $("#updates").load("/blog/updates.html");
         $("#updates-mobile").empty();
-    } else {
+    } else { // for small screens
         $("#updates-mobile").load("/blog/updates.html");
         $("#updates").empty();
     }
